72a7ae1149e5f8b99823d9406ae93af0fd8dbff4
Azaria <askelman5@gmail.com>
2024-09-23 10:15:51 -0400

    Implemented CheckStyle suggestions, implemented getKeyOneOfSecond

--- src/main/java/org/translation/InLabByHandTranslator.java
+++ src/main/java/org/translation/InLabByHandTranslator.java
@@ -13,6 +13,7 @@ import java.util.List;
  * An implementation of the Translator interface which translates
  * the country code "can" to several languages.
  */
+
 public class InLabByHandTranslator implements Translator {
     /**
      * Returns the language abbreviations for all languages whose translations are
@@ -21,17 +22,20 @@ public class InLabByHandTranslator implements Translator {
      * @param country the country
      * @return list of language abbreviations which are available for this country
      */
+
+    // TODO Checkstyle: Static variable definition in wrong order.
+    public static final String CANADA = "can";
+
+    private String can = "can";
+
     @Override
     public List<String> getCountryLanguages(String country) {
         // TODO Checkstyle: The String "can" appears 4 times in the file.
-        if ("can".equals(country)) {
+        if (can.equals(country)) {
             return new ArrayList<>(List.of("de", "en", "zh"));
         }
         return new ArrayList<>();
     }
-
-    // TODO Checkstyle: Static variable definition in wrong order.
-    public static final String CANADA = "can";
 
     /**
      * Returns the country abbreviations for all countries whose translations are
@@ -41,7 +45,7 @@ public class InLabByHandTranslator implements Translator {
      */
     @Override
     public List<String> getCountries() {
-        return new ArrayList<>(List.of("can"));
+        return new ArrayList<>(List.of(can));
     }
 
     /**
@@ -54,21 +58,19 @@ public class InLabByHandTranslator implements Translator {
     @Override
     public String translate(String country, String language) {
         // TODO Checkstyle: Return count is 5 (max allowed for non-void methods/ lambdas is 2).
-        // TODO Checkstyle: String literal expressions should be on the left side of an equals comparison
-        if (!country.equals("can")) {
+        // Checkstyle: String literal expressions should be on the left side of an equals comparison
+        if (!country.equals(can)) {
             return null;
         }
-        if (language.equals("de")) {
-            return "Kanada";
-        }
-        else if (language.equals("en")) {
-            return "Canada";
-        }
-        else if ("zh".equals(language)) {
-            return "加拿大";
-        }
-        else {
-            return null;
+        switch (language) {
+            case "de":
+                return "Kanada";
+            case "en":
+                return "Canada";
+            case "zh":
+                return "加拿大";
+            default:
+                return null;
         }
     }
 }
