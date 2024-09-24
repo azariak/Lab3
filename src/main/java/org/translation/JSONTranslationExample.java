42b1a01ab0f7b4719f3e4e627b84d9bee64b5d25
Azaria <askelman5@gmail.com>
2024-09-23 20:29:00 -0400

    implemented getCountryNameTranslation

--- src/main/java/org/translation/JSONTranslationExample.java
+++ src/main/java/org/translation/JSONTranslationExample.java
@@ -21,8 +21,6 @@ public class JSONTranslationExample {
         try {
             // this next line of code reads in a file from the resources folder as a String,
             // which we then create a new JSONArray object from.
-            // TODO CheckStyle: Line is longer than 120 characters
-            //                  (note: you can split a line such that the next line starts with a .method()... call
             String jsonString = Files.readString(Paths.get(getClass().getClassLoader().getResource("sample.json")
                     .toURI()));
             this.jsonArray = new JSONArray(jsonString);
@@ -39,13 +37,9 @@ public class JSONTranslationExample {
 
     public String getCanadaCountryNameSpanishTranslation() {
 
-        // TODO Checkstyle: '30' is a magic number.
         JSONObject canada = jsonArray.getJSONObject(CANADA_INDEX);
         return canada.getString("es");
     }
-
-    // TODO Task: Complete the method below to generalize the above to get the country name
-    //            for any country code and language code from sample.json.
 
     /**
      * Returns the name of the country based on the provided country and language codes.
@@ -54,7 +48,14 @@ public class JSONTranslationExample {
      * @return the translation of country to the given language or "Country not found" if there is no translation.
      */
     public String getCountryNameTranslation(String countryCode, String languageCode) {
+
+        for (int i = 0; this.jsonArray.length() >= i; i++) {
+            if (this.jsonArray.getJSONObject(i).getString("alpha3").equals(countryCode)) {
+                return this.jsonArray.getJSONObject(i).getString(languageCode);
+            }
+        }
         return "Country not found";
+
     }
 
     /**
